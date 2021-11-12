7867c2ba6353907b6e57ba1a2e85489c3bfa8a95
Terje Larsen <terlar@gmail.com>
2021-11-12 16:14:10 +0100

    lib/writeTerraformVersions: add function to write versions and lock file

--- flake.nix
+++ flake.nix
@@ -27,11 +27,68 @@
         });
     in
     {
+      lib = {
+        # Create a versions.tf.json file for given terraform and list of provider names.
+        # You either have to name a system or set pkgs.
+        writeTerraformVersions =
+          { system ? ""
+          , pkgs ? nixpkgsFor.${system}
+          , terraform
+          , providers ? [ ]
+          }:
+          let
+            filename = "versions.tf.json";
+
+            useDependencyLockfile =
+              lib.versionAtLeast (lib.getVersion terraform) "0.14.0";
+
+            config = {
+              terraform = {
+                required_version = lib.getVersion terraform;
+                required_providers = lib.genAttrs providers (name:
+                  let provider = pkgs.terraform-providers.${name};
+                  in
+                  {
+                    version = lib.getVersion provider;
+                    source = provider.provider-source-address;
+                  });
+              };
+            };
+          in
+          pkgs.stdenv.mkDerivation {
+            name = "versions-tf";
+
+            dontUnpack = true;
+            value = builtins.toJSON config;
+            passAsFile = [ "value" ];
+
+            nativeBuildInputs = [ pkgs.jq ] ++ lib.optional useDependencyLockfile
+              (terraform.withPlugins (p: map (name: p.${name}) providers));
+            buildPhase = ''
+              ls -la
+              jq . "$valuePath" > ${filename}
+              ${lib.optionalString useDependencyLockfile ''
+                terraform init -backend=false
+              ''}
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              cp ${filename} $out
+              ${lib.optionalString useDependencyLockfile ''
+                cp .terraform.lock.hcl $out
+              ''}
+            '';
+
+            passthru = { inherit config; };
+          };
+      };
+
       packages = forAllSystems (system: {
         inherit (nixpkgsFor.${system}) terraform_1_0;
         terraform-provider-aws = nixpkgsFor.${system}.terraform-providers.aws;
       });
-        
+
       apps = forAllSystems (system: {
         terraform = {
           type = "app";
@@ -68,8 +125,14 @@
         };
       };
 
-      checks = forAllSystems (system: {
-        inherit (self.packages.${system}) terraform_1_0 terraform-provider-aws;
-      });
+      checks = forAllSystems
+        (system: {
+          inherit (self.packages.${system}) terraform_1_0 terraform-provider-aws;
+          build-writeTerraformVersions = self.lib.writeTerraformVersions {
+            inherit system;
+            terraform = self.packages.${system}.terraform_1_0;
+            providers = [ "aws" ];
+          };
+        });
     };
 }
