6f23a79aaccf1a414f888935f73b6bc474adb48d
Terje Larsen <terlar@gmail.com>
2021-11-17 23:29:56 +0100

    lib/writeTerraformVersions: handle empty providers list

--- flake.nix
+++ flake.nix
@@ -31,15 +31,15 @@
         # Create a versions.tf.json file for given terraform and list of provider names.
         # You either have to name a system or set pkgs.
         writeTerraformVersions =
-          { system ? ""
-          , pkgs ? nixpkgsFor.${system}
-          , terraform
+          { terraform
           , providers ? [ ]
+          , system ? ""
+          , pkgs ? nixpkgsFor.${system}
           }:
           let
             filename = "versions.tf.json";
 
-            useDependencyLockfile =
+            useDependencyLockfile = providers != [ ] &&
               lib.versionAtLeast (lib.getVersion terraform) "0.14.0";
 
             config = {
@@ -129,20 +129,21 @@
         (system:
           let
             pkgs = nixpkgsFor.${system};
-            checkWriteTerraformVersions = { terraform, useLockFile ? true }:
+            checkWriteTerraformVersions = { version, providers ? [ ], useLockFile ? (providers != [ ]) }:
               let
-                drv = self.lib.writeTerraformVersions {
-                  inherit system terraform;
-                  providers = [ "aws" ];
-                };
+                terraform = pkgs."terraform_${version}";
+                drv = self.lib.writeTerraformVersions { inherit system terraform providers; };
               in
-              pkgs.runCommand "check-terraform-${lib.getVersion terraform}-versions"
-                { nativeBuildInputs = [ pkgs.jq (terraform.withPlugins (p: [ p.aws ])) ]; }
+              pkgs.runCommand "check-terraform-${version}-versions"
+                { nativeBuildInputs = [ pkgs.jq (terraform.withPlugins (ps: map (p: ps.${p}) providers)) ]; }
                 ''
                   cd ${drv}
                   [ -f versions.tf.json ] || false
                   ${lib.optionalString useLockFile "[ -f .terraform.lock.hcl ] || false"}
-                  [ "$(jq -r .terraform.required_providers.aws.version < versions.tf.json)" = "${lib.getVersion pkgs.terraform-providers.aws}" ] || false
+                  ${lib.concatMapStringsSep "\n" (provider: ''
+                    [ "$(jq -r .terraform.required_providers.${provider}.version < versions.tf.json)" = "${lib.getVersion pkgs.terraform-providers.${provider}}" ] || false
+                  '') providers}
+
                   [ "$(jq -r .terraform.required_version < versions.tf.json)" = "${lib.getVersion terraform}" ] || false
                   export TF_DATA_DIR="$(mktemp -d)/.terraform"
                   terraform init -backend=false
@@ -152,16 +153,21 @@
           {
             inherit (self.packages.${system}) terraform_1_0 terraform-provider-aws;
           } // lib.listToAttrs (map
-            (args@{ terraform, ... }: {
-              name = "writeTerraformVersions-with-${lib.getVersion terraform}";
+            (args@{ version, providers ? [ ], ... }: {
+              name = "writeTerraformVersions-with-${builtins.concatStringsSep "-" (providers ++ [ version ])}";
               value = checkWriteTerraformVersions args;
             })
             [
-              { terraform = pkgs.terraform_0_12; useLockFile = false; }
-              { terraform = pkgs.terraform_0_13; useLockFile = false; }
-              { terraform = pkgs.terraform_0_14; }
-              { terraform = pkgs.terraform_0_15; }
-              { terraform = pkgs.terraform_1_0; }
+              { version = "0_12"; }
+              { version = "0_13"; }
+              { version = "0_14"; }
+              { version = "0_15"; }
+              { version = "1_0"; }
+              { version = "0_12"; providers = [ "aws" ]; useLockFile = false; }
+              { version = "0_13"; providers = [ "aws" ]; useLockFile = false; }
+              { version = "0_14"; providers = [ "aws" ]; }
+              { version = "0_15"; providers = [ "aws" ]; }
+              { version = "1_0"; providers = [ "aws" ]; }
             ])
         );
     };
