b7606901f7bc2abe35ea60e7a8a26e0c65fd2788
Terje Larsen <terlar@gmail.com>
2023-12-13 01:19:10 +0100

    feat: support opentofu and non-free terraform

--- dev/tests.nix
+++ dev/tests.nix
@@ -1,60 +1,57 @@
 {
   lib,
   pkgs,
-  inputs',
   rootFlake',
   ...
 }: let
   nameForTest = {
-    version,
+    package,
     providers ? [],
     ...
-  }: "writeTerraformVersions-with-${builtins.concatStringsSep "+" ([version] ++ providers)}";
+  }: "writeTerraformVersions-with-${lib.pipe package [
+    (builtins.getAttr "name")
+    (builtins.replaceStrings ["."] ["_"])
+    lib.singleton
+    ((lib.flip lib.concat) providers)
+    (builtins.concatStringsSep "+")
+  ]}";
 
   checkWriteTerraformVersions = pkgs.callPackage ./check-write-terraform-versions.nix {
     inherit (rootFlake'.legacyPackages) writeTerraformVersions;
-    terraformPkgs = {
-      inherit
-        (inputs'.nixpkgs-21_11.legacyPackages)
-        terraform_0_12
-        terraform_0_13
-        terraform_0_14
-        terraform_0_15
-        ;
-      inherit
-        (pkgs)
-        terraform_1
-        ;
-    };
   };
 in {
   checks =
     lib.pipe [
-      {version = "0_12";}
-      {version = "0_13";}
-      {version = "0_14";}
-      {version = "0_15";}
-      {version = "1";}
+      {package = pkgs.terraform_0_12;}
+      {package = pkgs.terraform_0_13;}
+      {package = pkgs.terraform_0_14;}
+      {package = pkgs.terraform_0_15;}
+      {package = pkgs.terraform_1;}
+      {package = pkgs.opentofu;}
       {
-        version = "0_12";
+        package = pkgs.terraform_0_12;
         providers = ["aws"];
         useLockFile = false;
       }
       {
-        version = "0_13";
+        package = pkgs.terraform_0_13;
         providers = ["aws"];
         useLockFile = false;
       }
       {
-        version = "0_14";
+        package = pkgs.terraform_0_14;
         providers = ["aws"];
       }
       {
-        version = "0_15";
+        package = pkgs.terraform_0_15;
         providers = ["aws"];
       }
       {
-        version = "1";
+        package = pkgs.terraform_1;
+        providers = ["aws"];
+      }
+      {
+        package = pkgs.opentofu;
         providers = ["aws"];
       }
     ] [
