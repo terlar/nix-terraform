9d9a28617f6cc1b01cd0d70ba148e8974cdbd9a4
Terje Larsen <terlar@gmail.com>
2026-09-15 09:42:44 +0200

    chore: use nixos.org tarball for nixpkgs (#216)

--- dev/flake.nix
+++ dev/flake.nix
@@ -4,9 +4,9 @@
   inputs = {
     dev-flake.url = "github:terlar/dev-flake";
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
     # For Terraform packages pre 1.x
-    nixpkgs-21_11.url = "github:nixos/nixpkgs/release-21.11";
+    nixpkgs-21_11.url = "https://channels.nixos.org/nixos-21.11/nixexprs.tar.xz";
   };
 
   outputs = _: { };
