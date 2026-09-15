cd533fa8dd1ce02c79f1c5b013dd07c6d9c51c77
Terje Larsen <terlar@gmail.com>
2026-09-15 20:55:33 +0200

    chore: use nixos.org tarball for nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix+Terraform";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
     flake-parts.url = "github:hercules-ci/flake-parts";
     terranix = {
       url = "github:terranix/terranix";
